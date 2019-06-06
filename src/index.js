77e87a5287491ee4e00e9e25d2d895ced58d6f9d
Timothy Miller <tim@webinspect.tech>
2019-06-06 14:28:40 -0500

    Add functionality to handle multiple date entries

--- src/index.js
+++ src/index.js
@@ -11,23 +11,16 @@ const CSV_PATH = `${__dirname}/../example.csv`;
 const MARKDOWN_DIR = `${__dirname}/../markdown-posts`;
 
 // Filenames of Markdown files
-const getMarkdownPath = post => `${MARKDOWN_DIR}/${post.Slug}.md`;
+const getMarkdownPath = post => `${MARKDOWN_DIR}/${post.published_on}.md`;
 
 // Template for Markdown files
 const renderMarkdown = post => dedent`
-  ---
-  title: "${post.Title}"
-  excerpt: "${post.Excerpt}"
-  author: "${post.Author || "Example default"}"
-  categories: "${post.Categories}"
-  date: "${formatDate(post["Published date"])}"
-  path: "/blog/${post.Slug}"
-  ---
-
-  ${post.Content}
+  ${post.body}\n\n
 `;
 
 const stream = createReadStream(CSV_PATH);
+
+let dates_done = [];
 
 const parser = parse({ columns: true }, (error, posts) => {
   if (error) throw new Error(error);
@@ -36,13 +29,24 @@ const parser = parse({ columns: true }, (error, posts) => {
   mkdirp(MARKDOWN_DIR);
 
   posts.forEach(post => {
-    const convertedPost = renderMarkdown(post);
-    const filepath = getMarkdownPath(post);
+    let file_index = post.completed_on;
 
-    writeFile(filepath, convertedPost, error => {
-      if (error) throw new Error(`Could not create ${filepath}`);
-      console.log(`Created ${filepath}`);
-    });
+    if (dates_done.indexOf(file_index) === -1) {
+      let convertedPost = '';
+      posts.forEach(post1 => {
+        if (post1.completed_on === file_index) {
+          convertedPost = renderMarkdown(post1) + convertedPost;
+        }
+      });
+
+      const filepath = getMarkdownPath(post);
+      dates_done.push(file_index);
+
+      writeFile(filepath, convertedPost, error => {
+        if (error) throw new Error(`Could not create ${filepath}`);
+        console.log(`Created ${filepath}`);
+      });
+    }
   });
 });
 
